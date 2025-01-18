2bface63865fbc5b78636356139606e76a79fe34
L7DyGeras <pauloricardomarinhll@gmail.com>
2025-01-17 21:39:21 -0400

    Add files via upload

--- bdd.sql
+++ bdd.sql
@@ -2,9 +2,10 @@ CREATE DATABASE floresta_verde;
 
 USE floresta_verde;
 
-CREATE TABLE contatos (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(255),
-    email VARCHAR(255),
-    mensagem TEXT
+CREATE TABLE mensagens (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  nome VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  mensagem TEXT NOT NULL,
+  data_envio TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
